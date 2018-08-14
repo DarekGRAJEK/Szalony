e073abb855c268e0fc7eaf591b5716f48b4d5d95
DarekGRAJEK <41697254+DarekGRAJEK@users.noreply.github.com>
2018-08-14 22:47:05 +0200

    423423423

--- commands/8-ball.js
+++ commands/8-ball.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
      
+    await message.delete();
     if(!args[2]) return message.reply("Please ask a full question")
     let replies = ["Tak.", "Nie.", "Może.", "Nie wiem", "Napewno Tak.", "Napewno Nie.", "Nie Moge Odpowiedzieć na to pytanie."];
 
